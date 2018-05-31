7ad5cbb778c4b8a05c4bb7e08934b53cee68c589
Taggart Bonham <taggartbonham@yahoo.com>
2018-05-31 15:30:51 -0400

    Syncronized to beat

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@
   Based on examples from: https://p5js.org/
  */
 
-let mic, soundFile, fft;
+let mic, amplitude, soundFile, fft;
 const smoothing = 0.93;
-const binCount = 256;
+const binCount = 512;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -24,7 +24,7 @@ function setup() {
 function draw() {
   // returns an array with [binCount] amplitude readings from lowest to highest frequencies
   const spectrum = fft.analyze(binCount);
-  // background(spectrum[0],spectrum[0],spectrum[0]);
+  // background(255 - spectrum[0], 255 - spectrum[0], 255 - spectrum[0]);
   background(255);
   // background(map(spectrum[0], 0, 255, 240, 0));
 
@@ -32,18 +32,16 @@ function draw() {
     push();
     for (let i = 0; i < 90; i += 1) {
       const thisLevel = map(spectrum[2 * i], 0, 255, -1, 1);
-      const thisLevel2 = map(spectrum[0], 0, 255, -1, 1);
-      translate(sin((frameCount * 0.0001) + j + thisLevel) * 100, sin((frameCount * 0.0001) + j + thisLevel) * 100, i * 0.1);
-      rotateZ((frameCount * 0.002) + thisLevel2);
+      const thisLevel2 = map(spectrum[binCount - 1], 0, 255, -1, 1);
+      // translate(sin((frameCount * 0.0001) + j + thisLevel) * 100, sin((frameCount * 0.0001) + j + thisLevel) * 100, i * 0.1);
+      translate(sin((frameCount * 0.0001) + j + thisLevel) * 100, cos((frameCount * 0.0001) + j + thisLevel) * 100, i * 0.1);
+      rotateZ(sin(frameCount * 0.0002) + thisLevel2);
       push();
       sphere(map(spectrum[2 * i], 0, 255, 2, 5));
-      // var color = map(spectrum[i], 0, 255, 255, 0);
-      // var opacity = map(mic.getLevel(), 0, 1, 0, 255);
-      // console.log(mic.getLevel());
 
       const opacity = map(spectrum[0], 0, 255, 0.2, 1);
       pop();
-      fill(`rgba(${0},${spectrum[2 * i]},${255 - spectrum[2 * i]},${opacity})`);
+      fill(`rgba(${spectrum[200]},${spectrum[2 * i]},${255 - spectrum[2 * i]},${opacity})`);
     }
     pop();
   }
