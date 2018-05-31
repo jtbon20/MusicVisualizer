778ab414b25eb8ab3d9189efc85a6f4b422b5263
Taggart Bonham <taggartbonham@yahoo.com>
2018-05-31 13:50:48 -0400

    Added linting

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+/* eslint no-unused-vars: 0 no-undef:0 */
+
 /*
   Based on examples from: https://p5js.org/
  */
@@ -25,41 +27,27 @@ function draw() {
   const spectrum = fft.analyze(binCount);
   // background(spectrum[0],spectrum[0],spectrum[0]);
   background(255);
-    // background(map(spectrum[0], 0, 255, 240, 0));
+  // background(map(spectrum[0], 0, 255, 240, 0));
 
-  for(var j = 0; j < 20; j++){
+  for (let j = 0; j < 20; j += 1) {
     push();
-    for(var i = 0; i < 90; i++){
-      var thisLevel = map(spectrum[2*i], 0, 255, -1, 1);
-            var thisLevel2 = map(spectrum[0], 0, 255, -1, 1);
-    translate(sin(frameCount * 0.0001 + j + thisLevel) * 100, sin(frameCount * 0.0001 + j + thisLevel) * 100, i * 0.1);
-      rotateZ(frameCount * .002 + thisLevel2);
+    for (let i = 0; i < 90; i += 1) {
+      const thisLevel = map(spectrum[2 * i], 0, 255, -1, 1);
+      const thisLevel2 = map(spectrum[0], 0, 255, -1, 1);
+      translate(sin((frameCount * 0.0001) + j + thisLevel) * 100, sin((frameCount * 0.0001) + j + thisLevel) * 100, i * 0.1);
+      rotateZ((frameCount * 0.002) + thisLevel2);
       push();
-      sphere(map(spectrum[2*i], 0, 255, 2, 5));
-      //var color = map(spectrum[i], 0, 255, 255, 0);
-      //var opacity = map(mic.getLevel(), 0, 1, 0, 255);
+      sphere(map(spectrum[2 * i], 0, 255, 2, 5));
+      // var color = map(spectrum[i], 0, 255, 255, 0);
+      // var opacity = map(mic.getLevel(), 0, 1, 0, 255);
       // console.log(mic.getLevel());
 
-      var c = color(255,0,0);
-      var opacity = map(spectrum[0], 0, 255, .2, 1);
+      const c = color(255, 0, 0);
+      const opacity = map(spectrum[0], 0, 255, 0.2, 1);
       pop();
-            fill(`rgba(${0},${spectrum[2*i]},${255-spectrum[2*i]},${opacity})`);
+      fill(`rgba(${0},${spectrum[2 * i]},${255 - spectrum[2 * i]},${opacity})`);
     }
     pop();
-  }
-}
-// ================
-// Helper Functions
-// ================
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  background(0);
-}
-
-function keyPressed() {
-  if (key === 'T') {
-    toggleInput();
   }
 }
 
@@ -74,5 +62,20 @@ function toggleInput() {
     soundFile.play();
     mic.stop();
     fft.setInput(soundFile);
+  }
+}
+
+// ================
+// Helper Functions
+// ================
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function keyPressed() {
+  if (key === 'T') {
+    toggleInput();
   }
 }
